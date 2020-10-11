06949cc5266fb1b1fa8c01e439b30118f41a13f8
Skinz3 <oliviamarius35@gmail.com>
2020-10-11 18:56:37 +0200

    Ajout du test de thomas. (liste & ordre)

--- src/main/java/test/Program.java
+++ src/main/java/test/Program.java
@@ -10,7 +10,9 @@ import java.util.List;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 
 public class Program 
@@ -67,6 +69,37 @@ public class Program
 			assertEquals(instance.getIndex(),mock.getIndex());
 		}
 	}
+	
+	@Test
+    public void list_test()
+	{
+        MyClass cMock = mock(MyClass.class);
+        MyClass cMock2 = mock(MyClass.class);
+
+        InOrder order = Mockito.inOrder(cMock);
+        InOrder order2 = Mockito.inOrder(cMock2);
+
+        cMock.addToList(1);
+
+        cMock.addToList(2);
+        cMock.addToList(2);
+        cMock2.addToList(1);
+        cMock2.addToList(1);
+        cMock2.addToList(2);
+
+        when(cMock.getFromList(0)).thenReturn(1);
+        when(cMock.getFromList(3)).thenThrow(new RuntimeException());
+
+        System.out.println(cMock.getFromList(0));
+        InOrder order3 = inOrder(cMock, cMock2);
+        //Verifie l'ordre et le nombres d'appels
+        order3.verify(cMock, times(1)).addToList(1);
+        order3.verify(cMock, times(2)).addToList(2);
+        order3.verify(cMock2, times(2)).addToList(1);
+        order3.verify(cMock2, times(1)).addToList(2);
+
+        verify(cMock, times(0)).addToList(0);
+    }
 	
 	 
 	
